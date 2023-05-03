26a25de1a35adb7990538457d9f9068269107694
tianyimasf <tianyi437@gmail.com>
2023-05-03 00:13:19 -0600

    change to live server

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from helper import absmag2rad, bvToRgb, bv2rgb, const_abbr2full
 
 app = Dash(__name__)
 
-# server = app.server
+server = app.server
 
 # -- Import and clean data (importing csv into pandas)
 # TODO: 
@@ -267,5 +267,5 @@ app.layout = html.Div(
 
 
 # ------------------------------------------------------------------------------
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
